3826b206f8780ec2d04f759e32a3f923715c672b
YasserB94 <yasserbal@icloud.com>
2022-09-21 14:51:19 +0200

    Refactored Server class Now has proper encapsulation Middleware will be set in init() method Listen is now a public method of Server class

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,33 +14,39 @@ dotenv.config({
 });
 //Create a Server class that contains everything our server needs
 class Server {
-	public app = express();
-	public router = MasterRouter;
+	private app = express();
+	private router = MasterRouter;
+	private errorHandler = ErrorHandler;
+	public init(){
+		//Add MasterRouter
+		this.app.use('/api',this.router)
+		//Integrate Errorhandler model
+		this.initializeErrorhandler();
+		//Make server Listen
+		this.listen();
+	}
+	private initializeErrorhandler(){
+		this.app.use((err: ErrorHandler, req: express.Request, res: express.Response, next: express.NextFunction) => {
+			res.status(err.statusCode || 500).json({
+			  status: 'error',
+			  statusCode: err.statusCode,
+			  message: err.message
+			});
+		  })
+	}
+	private listen() {
+		const port =  process.env.APP_PORT || 5000;
+		this.app.listen(port,():void =>{
+			console.log(`Server Running on: ${port}`)
+			console.log(`127.0.0.1:${port}`)
+			if(port===5000){
+				console.warn(`5000 is the fallback port for the server, your environment variables might not be configured.`)
+				console.log(`To set the server port use: "APP_PORT=XXXX"`)
+			}
+		})
+	}
+	
   }
 //Initialize the server.
 const server = new Server();
-//Routing all traffic to /api towards our master router
-server.app.use('/api', server.router);
-//Add Errorhandler MiddleWare - For now I will use the global express import to get the routing methods from express
-server.app.use((err: ErrorHandler, req: express.Request, res: express.Response, next: express.NextFunction) => {
-	res.status(err.statusCode || 500).json({
-	  status: 'error',
-	  statusCode: err.statusCode,
-	  message: err.message
-	});
-  });
-//Create a function that makes our server listen to http requests
-//The magic number '5000' is a fallback in case of missing .env
-const listen = (port = process.env.APP_PORT||5000):void=>{
-	server.app.listen(port,():void =>{
-		//This is a callback function executed when server boots up.
-		console.log(`FriendShip-Book Express App is listening on port ${port}`)
-		console.log(`127.0.0.1:${port}`)
-		//Let's tell users if their .env port is not being used
-		if(port===5000){
-			console.warn(`EXPRESS: Currently using fallback port 5000. Please check if your .env file is properly loading!`)
-		}
-	})
-}
-//Run our 
-listen();
+server.init();
