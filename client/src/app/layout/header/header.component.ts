b0bc956612dd63c4c0ba2de03118a6fa9e7393d7
Yasser Bal <yasserbal@icloud.com>
2022-09-26 15:23:41 +0200

    Added Responsive navbar

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -5,7 +5,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  navOpen = false;
   constructor() {}
 
   ngOnInit(): void {}
+  toggleNav() {
+    this.navOpen = !this.navOpen;
+  }
 }
+
+/*
+animations: [
+    trigger('toggleBurgerCloseAnimation', [
+      state('closed', style({})),
+      state(
+        'openTopLine',
+        style({
+          transform: 'rotate(45deg)',
+        })
+      ),
+      state(
+        'openMiddleLine',
+        style({
+          opacity: '0',
+        })
+      ),
+      state(
+        'openBottomLine',
+        style({
+          transform: 'rotate(-45deg)',
+        })
+      ),
+    ]),
+  ]
+  */
--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, MatToolbarModule, AppRoutingModule],
+  imports: [CommonModule, MatToolbarModule, AppRoutingModule, MatButtonModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
